dc63651a922e3408d59df151687db16017907155
nicomazz <nicolmazzucato@me.com>
2017-10-12 00:48:43 +0200

    Added updated php files

---
+++ php/cacher_utils.php
@@ -0,0 +1,7 @@
+<?php
+// define the path and name of cached file
+	$cachefile = 'cached-files/cached_esu.json';
+	// define how long we want to keep the file in seconds. I set mine to 5 hours.
+	$cachetime = 60*5; // 5 minuti
+
+?>
---
+++ php/docache.php
@@ -0,0 +1,35 @@
+<?php
+require_once __DIR__ . '/cacher_utils.php';
+	
+	// Check if the cached file is still fresh. If it is, serve it up and exit.
+	if (file_exists($cachefile) && time() - $cachetime < filemtime($cachefile)) {
+		//include($cachefile);
+		//files already present and updated
+		exit;
+	}
+	ignore_user_abort(true);
+	set_time_limit(0);
+	
+	ob_start();
+	// do initial processing here
+	echo "caching in progress!"; // send the response
+	header('Connection: close');
+	header('Content-Length: '.ob_get_length());
+	ob_end_flush();
+	ob_flush();
+	flush();
+
+
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_URL, 
+	    "http://mobile.esupd.gov.it/api/reiservice.svc/canteens?lang=it"
+	);
+	$content = curl_exec($ch);
+
+	// We're done! Save the cached content to a file
+	$fp = fopen($cachefile, 'w');
+	fwrite($fp, $content);
+	fclose($fp);
+	// finally send browser output
+?>
